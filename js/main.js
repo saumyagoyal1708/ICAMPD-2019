5c568c108efff788cf85ff53337bc766c534b079
Axle10 <saptarshi.sinha.ind@gmail.com>
2019-05-27 02:29:01 +0530

    content updated

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("18 October 2019 00:00:00 GMT+05:30");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
